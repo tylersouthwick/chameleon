cddb290d9fc22d5c2bf6791a5c39874c43836799
Tyler Southwick <tyler@northfuse.net>
2012-01-30 20:28:48 -0700

    added jetty runner

--- src/main/scala/net/northfuse/scont/JettyRunner.scala
+++ src/main/scala/net/northfuse/scont/JettyRunner.scala
@@ -1,11 +1,17 @@
 package net.northfuse.scont
 
+import org.mortbay.jetty.servlet.{ServletHolder, Context}
+import org.mortbay.jetty.Server
+
 /**
  * @author tylers2
  */
 
 object JettyRunner {
 	def main(args : Array[String]) {
-		val server = new org.mortbay.jetty.Server(8080);
+		val server = new Server(8080)
+    val root = new Context(server, "/", Context.SESSIONS)
+    root.addServlet(new ServletHolder(new ScontServlet), "/*")
+    server.start()
 	}
 }
