47e8629eb149fc1530f7caf53f18efe149c73ba1
Tyler Southwick <tyler@northfuse.net>
2012-02-01 09:59:28 -0700

    use delta queue for session

--- src/main/scala/net/northfuse/scont/ScontSession.scala
+++ src/main/scala/net/northfuse/scont/ScontSession.scala
@@ -7,41 +7,43 @@ import java.lang.ThreadLocal
 /**
  * @author tylers2
  */
-class ScontSession(session : Session, request : Request) {
+class ScontSession(session: Session, request: Request) {
 
 	import ScontSession.ScontCallback
-  
-	def add(callback : ScontCallback) = {
+
+	def add(callback: ScontCallback) = {
 		val identifier = newIdentifier
 		addToSession(identifier, callback)
 		identifier
 	}
-	
-  def map = {
-    val m = session.getAttribute("scont_session")
 
-    if (m == null) {
-      val newM = new collection.mutable.HashMap[String, ScontCallback]
-      session.setAttribute("scont_session", newM)
-      newM
-    } else {
-      m.asInstanceOf[collection.mutable.HashMap[String, ScontCallback]]
-    }
-  }
+	def map = lock {
+		val m = session.getAttribute("scont_session")
 
-	def addToSession(identifier : String,  callback : ScontCallback) {
-    map.put(identifier, callback)
+		if (m == null) {
+			val newM = new DeltaQueueMap[String, ScontCallback](10)
+			session.setAttribute("scont_session", newM)
+			newM
+		} else {
+			m.asInstanceOf[DeltaQueueMap[String, ScontCallback]]
+		}
 	}
-	
-	def getFromSession(identifier : String) = {
+
+	def addToSession(identifier: String, callback: ScontCallback) {
+		lock {
+			map.put(identifier, callback)
+		}
+	}
+
+	def getFromSession(identifier: String) = lock {
 		map.get(identifier) match {
-      case Some(callback) => callback
-      case None => throw new IdentifierNotFoundException(identifier)
-    }
+			case Some(callback) => callback
+			case None => throw new IdentifierNotFoundException(identifier)
+		}
 	}
-	
+
 	def newIdentifier = java.util.UUID.randomUUID().toString
-	
+
 	val currentIdentifier = {
 		val id = {
 			val pathInfo = request.getPathInfo
@@ -51,13 +53,19 @@ class ScontSession(session : Session, request : Request) {
 		}
 		if (id == null || id.trim().isEmpty) None else Some(id)
 	}
-	
+
 	def current = currentIdentifier match {
 		case None => None
 		case Some(identifier) => Some(getFromSession(identifier))
 	}
-	
-	def all = map.keys
+
+	def all = lock {map.keys}
+
+	def lock[T](callback : => T) = {
+		session synchronized {
+			callback
+		}
+	}
 }
 
 object ScontSession {
@@ -66,10 +74,10 @@ object ScontSession {
 	private val holder = new ThreadLocal[ScontSession]
 
 	def session = holder.get()
-	
-	def apply(callback : ScontCallback) = session.add(callback)
-	
-	def apply(request : Request, response : Response, start : => Unit, errorHandle : PartialFunction[Throwable, Unit]) {
+
+	def apply(callback: ScontCallback) = session.add(callback)
+
+	def apply(request: Request, response: Response, start: => Unit, errorHandle: PartialFunction[Throwable, Unit]) {
 		val session = new ScontSession(request.getSession, request)
 		holder.set(session)
 		try {
@@ -90,4 +98,4 @@ object ScontSession {
 	}
 }
 
-class IdentifierNotFoundException(val identifier : String) extends RuntimeException(identifier + " not found")
+class IdentifierNotFoundException(val identifier: String) extends RuntimeException(identifier + " not found")
