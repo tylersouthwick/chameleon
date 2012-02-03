f4a82c13f0b6bfd13468b28d3d8aa83ac9a81f63
Tyler Southwick <tyler@northfuse.net>
2012-02-03 14:18:16 -0700

    cleaned up method security on SpringServlet

--- core/src/main/scala/net/northfuse/chameleon/SpringBeanFinder.scala
+++ core/src/main/scala/net/northfuse/chameleon/SpringBeanFinder.scala
@@ -8,23 +8,22 @@ import javax.servlet.{Servlet, ServletConfig, ServletContext}
  * @author Tyler Southwick
  */
 trait SpringBeanFinder {
-	def findBean[T](klass : Class[T]) = SpringBeanFinder.applicationContext.getBean(klass)
+	def findBean[T](klass : Class[T]) = SpringServlet.findBean(klass)
 }
 
 trait SpringServlet extends Servlet {
 	abstract override def init(config: ServletConfig) {
 		super.init(config)
-		SpringBeanFinder.context_=(config.getServletContext)
+		SpringServlet.context_=(config.getServletContext)
 	}
 }
 
-object SpringBeanFinder {
+object SpringServlet {
 	private var applicationContext : ApplicationContext = null
 
-	def context_=(servletContext : ServletContext) {
+	private def context_=(servletContext : ServletContext) {
 		applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext)
 	}
 
-	def findBean[T](implicit klass : Class[T]) = applicationContext.getBean(klass)
-
+	def findBean[T](klass : Class[T]) = SpringServlet.applicationContext.getBean(klass)
 }
